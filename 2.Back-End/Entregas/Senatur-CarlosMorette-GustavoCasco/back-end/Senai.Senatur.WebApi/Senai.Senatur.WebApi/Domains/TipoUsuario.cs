ac73805edf9e29811213c1bae337e0c4770e4e41
CarlosMorette <carlos.morette@outlook.com>
2020-04-10 13:45:33 -0300

    Adicionado entregas

---
+++ 2.Back-End/Entregas/Senatur-CarlosMorette-GustavoCasco/back-end/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Domains/TipoUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Senai.Senatur.WebApi.Domains
+{
+    public partial class TipoUsuario
+    {
+        public TipoUsuario()
+        {
+            Usuario = new HashSet<Usuario>();
+        }
+
+        public int IdTipoUsuario { get; set; }
+        public string Titulo { get; set; }
+
+        public ICollection<Usuario> Usuario { get; set; }
+    }
+}
---
+++ 2.Back-End/Entregas/Senatur-CarlosMorette-GustavoCasco/back-end/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Interfaces/IUsuariosRepository.cs
@@ -0,0 +1,23 @@
+﻿using Senai.Senatur.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.Interfaces
+{
+    interface IUsuariosRepository
+    {
+        List<Usuario> Listar();
+
+        void Cadastrar(Usuario novoUser);
+
+        void Atualizar(int Id, Usuario attUser);
+
+        void Deletar(int Id);
+
+        Usuario BuscarPorId(int id);
+
+        Usuario BuscarSenhaEmail(string email, string senha);
+    }
+}
